c8f16982674d2d73b6cc89f2b2f587b41413e63a
Joao Barreto <JoaoBarreto255@users.noreply.github.com>
2024-03-16 00:32:17 -0300

    improve: add cs-fixer

---
+++ .php-cs-fixer.php
@@ -0,0 +1,13 @@
+<?php
+
+// require 'vendor/autoload.php';
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__);
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@PSR12' => true,
+        // 'strict_param' => true,
+        'array_syntax' => ['syntax' => 'short'],
+    ])->setFinder($finder);
