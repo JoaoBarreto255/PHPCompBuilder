e167eb6ef2d5095fd81181901fca0b4912bf71f0
Joao Barreto <JoaoBarreto255@users.noreply.github.com>
2024-03-15 23:53:24 -0300

    test: add general test

--- tests/Lexer/Iterators/StringIteratorTest.php
+++ tests/Lexer/Iterators/StringIteratorTest.php
@@ -5,14 +5,16 @@ declare(strict_types=1);
 namespace Joaobarreto255\PhpCompBuilder\Tests\Lexer\Iterators;
 
 use Joaobarreto255\PhpCompBuilder\Lexer\Iterators\StringIterator;
+use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\TestDox;
 use PHPUnit\Framework\TestCase;
 
+#[CoversClass(StringIterator::class)]
 class StringIteratorTest extends TestCase
 {
     #[DataProvider('getSizeDataProvider')]
-    #[TestDox('Test GetSize of "$string" to be $expected.')]
+    #[TestDox('Test method getSize. expect for "$string" result be $expected.')]
     public function testGetSize(int $expected, string $input)
     {
         $it = new StringIterator($input);
@@ -29,26 +31,48 @@ class StringIteratorTest extends TestCase
         return $result;
     }
 
-    public function testCurrentAndNext()
+    #[DataProvider('generalDataProvider')]
+    #[TestDox('Test methods current, next, key, rewind, previous together')]
+    public function testGeneral(StringIterator $it, array $expectedValues, array $expectedKeys)
     {
-        $it = new StringIterator("abc");
-        $this->assertEquals('a', $it->current());
-        $it->next();
-        $this->assertEquals('b', $it->current());
-        $it->next();
-        $this->assertEquals('c', $it->current());
-        $it->next();
-        $this->assertNull($it->current());
+        # test next.
+        $zipped = array_map(null, $expectedValues, $expectedKeys);
+        foreach($zipped as [$value, $key]) {
+            $this->assertSame($value, $it->current());
+            $this->assertSame($key, $it->key());
+            $it->next();
+        }
 
-        $it = new StringIterator("abc", true);
-        $this->assertEquals('c', $it->current());
-        $it->next();
-        $this->assertEquals('b', $it->current());
-        $it->next();
-        $this->assertEquals('a', $it->current());
-        $it->next();
-        $this->assertNull($it->current());
+        // test rewind.
+        $it->rewind();
+        foreach($zipped as [$value, $key]) {
+            $this->assertSame($value, $it->current());
+            $this->assertSame($key, $it->key());
+            $it->next();
+        }
 
+        // test previous.
+        foreach(array_reverse($zipped) as [$value, $key]) {
+            $it->previous();
+            $this->assertSame($value, $it->current());
+            $this->assertSame($key, $it->key());
+        }
+    }
+
+    public static function generalDataProvider(): array
+    {
+        return [
+            "data set #1 - abc" => [
+                new StringIterator('abc'),
+                ["a", "b", "c", null],
+                [0, 1, 2, 3],
+            ],
+            "data set #2 - reverse abc" => [
+                new StringIterator('abc', true),
+                ["c", "b", "a", null],
+                [2, 1, 0, -1],
+            ],
+        ];
     }
 
 }
