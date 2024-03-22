edec197001c523c09834d4174d5cf9c73cd0b089
Joao Barreto <JoaoBarreto255@users.noreply.github.com>
2024-03-22 06:04:01 -0300

    improve: better code division

--- src/Lexer/Pattern/BuilderHandler.php
+++ src/Lexer/Pattern/BuilderHandler.php
@@ -33,10 +33,6 @@ class BuilderHandler
         63 => 'symbolMayNotHappen',
     ];
 
-    private int $classStart = 0;
-    private int $bracketsStack = 0;
-    private int $braceStart = 0;
-
     protected StringIterator $iterator;
 
     private array $symbols = [];
@@ -49,11 +45,16 @@ class BuilderHandler
 
     public function processPattern()
     {
-        // for \{symbols}
-        foreach ($this->iterator as $key => $char) {
-            $code = ord($char);
+        while ($this->iterator->valid()) {
+            $key = $this->iterator->key();
+            $char = $this->iterator->current();
+            $code = $this->iterator->currentCode();
+
             if (static::BAR === $code) {
-                $forceSymbol = true;
+                $this->iterator->next();
+                $char = $this->iterator->current() ?? '';
+                $this->symbols[] = UniqueSymbol::newFrom($char);
+                $this->iterator->next()
                 continue;
             }
 
@@ -74,13 +75,9 @@ class BuilderHandler
                 continue;
             }
 
-            if ($this->processClassPattern()) {
-                continue;
-            }
+            $this->processClassPattern();
 
-            if ($this->processBracePattern()) {
-                continue;
-            }
+            $this->processBracePattern();
 
             if ($this->processGroupPattern()) {
                 continue;
@@ -89,94 +86,115 @@ class BuilderHandler
 
     }
 
-    public function processClassPattern(): bool
+    public function processClassPattern(): void
     {
-        $code = $this->iterator->currentCode();
-        $key = $this->iterator->key();
-
-        if (static::CODE_DOT === $code &&  0 === $this->classStart) {
-            $this->symbols[] = ClassSymbol::newFrom(substr($this->pattern, $this->classStart, $key));
-
-            return true;
+        if (static::CODE_DOT === $this->iterator->currentCode()) { 
+            $this->symbols[] = ClassSymbol::newFrom($this->iterator->current());
+            $this->iterator->next();
         }
 
-        if (static::CODE_OPEN_CLASS === $code && 0 < $this->classStart) {
-            $this->classStart = $key + 1;
-
-            return true;
-        }
-
-        if (static::CODE_OPEN_CLASS === $code) {
-            throw new LogicException("Unexpected \"[\" at pattern position: $key", $key);
-        }
-
-        if ($this->classStart
-            && static::CODE_CLOSE_CLASS === $code
-            && $segment = substr(
-                $this->pattern,
-                $this->classStart,
-                $key - $this->classStart,
-            )
-        ) {
-            $this->symbols[] = ClassSymbol::newFrom($segment);
-            $this->classStart = 0;
-
-            return true;
-        }
-
-        if (static::CODE_CLOSE_CLASS === $code) {
+        if (static::CODE_CLOSE_CLASS === ($code = $this->iterator->currentCode())) {
             throw new LogicException("Unexpected \"]\" at pattern position: $key", $key);
         }
 
-        return  0 < $this->classStart;
+        if (static::CODE_OPEN_CLASS !== $code) { return null; }
+
+        $startPos = $this->iterator->key() + 1;
+        $this->iterator->next();
+        $len = 0;
+        while ($this->iterator->valid()) {
+            $pos = $this->iterator->key();
+            switch ($this->iterator->currentCode()) {
+                case static::CODE_BAR:
+                    $this->iterator->next();
+                    break;
+                case static::CODE_OPEN_CLASS:
+                    throw new LogicException("Unexpected \"[\" at pattern position: $pos", $pos);
+
+                case static::CODE_CLOSE_CLASS:
+                    $this->symbols[] = ClassSymbol::newFrom(substr($this->pattern, $startPos, $len));
+                    return;
+                
+                default:
+                    # do nothing
+                    break;
+            }
+
+            $len++;
+            $this->iterator->next();
+        }
+
+        throw new LogicException("Unexpected end of pattern! Expecting \"]\"", 1);
     }
 
-    public function processBracePattern(): bool
+    public function processBracePattern(): void
     {
         $code = $this->iterator->currentCode();
         $key = $this->iterator->key();
 
-        if (static::CODE_OPEN_BRACE === $code && 0 < $this->braceStart) {
-            $this->braceStart = $key + 1;
-    
-            return true;
+        if (static::CODE_CLOSE_BRACE === $code) { 
+            throw new LogicException(sprintf('Founded "]" with no previous "[" at position %d!', $key), $key);
         }
 
-        if (static::CODE_OPEN_BRACE === $code) {
-            throw new LogicException("Unexpected \"{\" at pattern position: $key", $key);
+        if (static::CODE_OPEN_BRACE !== $code) {
+            return;
         }
 
-        if (
-            $this->braceStart
-            && static::CODE_CLOSE_BRACE === $code
-            && ($segment = substr(
-                $this->pattern,
-                $this->braceStart,
-                $key - $this->braceStart,
-            ))
-            && $args = explode(',', $segment)
-        ) {
-            $args = array_map(fn($i) => (int) $i, $args);
-            if (!count($this->symbols)) {
-                throw new LogicException("Unexpected \"$char\" repeat operation", $key);
+        $startPosition = $key + 1;
+        $this->iterator->next();
+        $numbers = [''];
+        while ($this->iterator->valid()) {
+            $pos = $this->iterator->key();
+            switch ($this->iterator->current()) {
+                case '{':
+                    throw new LogicException("Unexpected \"{\" at position $pos", $pos);
+                
+                case '}':
+                    if (0 === count($numbers)) {
+                        throw new LogicException("Empty set of repeatitions! position $pos", $pos);
+                    }
+
+                    if (0 === count($this->symbols)) {
+                        throw new Exception("You cannot apply defined repeatition on no regex!", $startPos);
+                    }
+
+                    if (1 === count($numbers)) {
+                        $lastSymbol = array_pop($this->symbols);
+                        $this->symbols[] = $lastSymbol->symbolWillRepeatNTimes((int) $numbers[0]);
+                        $this->iterator->next();
+
+                        return;
+                    }
+
+                    $lastSymbol = array_pop($this->symbols);
+                    $this->symbols[] = $lastSymbol->symbolWillRepeatNTimes((int) $numbers[0], (int) $numbers[0]);
+                    $this->iterator->next();
+
+                    return;
+
+                case ',':
+                    $numbers[] = '';
+
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    $numbers[count($num)] .= $this->iterator->current();
+
+                default:
+                    throw new LogicException("Invalid repeatition symbol: ". $this->iterator->current(), 1);
             }
-            $symbol = array_pop($this->symbols);
-            if (1 === count($args)) {
-                $this->symbols[] = $symbol->symbolWillRepeatNTimes($args[0]);
-
-                return true;
-            }
-
-            $this->symbols[] = $symbol->symbolWillRepeatFromNToMTimes($args[0], $args[1]);
-
-            return true;
+            $this->iterator->next();
         }
 
-        if (static::CODE_CLOSE_BRACE === $code) {
-            throw new LogicException("Unexpected \"}\" at pattern position: $key", $key);
-        }
-
-        return 0 < $this->braceStart;
+        
+        throw new LogicException('Unexpected end of pattern! Waiting for "}"', 1);
     }
 
     public function processGroupPattern(): bool
