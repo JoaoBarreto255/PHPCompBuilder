87fd115ca197e0c06031d4c14ce99bb6b853f09e
Joao Barreto <JoaoBarreto255@users.noreply.github.com>
2024-03-29 01:14:08 -0300

    test: test for TokenRuleIterator

---
+++ tests/Lexer/Pattern/TokenRuleIteratorTest.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Joaobarreto255\PhpCompBuilder\Tests\Lexer\Pattern;
+
+use Joaobarreto255\PhpCompBuilder\Lexer\Pattern\TokenRuleIterator;
+use Joaobarreto255\PhpCompBuilder\Lexer\Pattern\TokenRulePattern;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\TestDox;
+use PHPUnit\Framework\TestCase;
+
+
+#[CoversClass(TokenRuleIterator::class)]
+class TokenRuleIteratorTest extends TestCase
+{
+    #[TestDox('Test iterate over matches')]
+    #[DataProvider('dataProvider')]
+    public function testIterateMatches(TokenRulePattern $pattern, string $input, array $expected)
+    {
+        $iterator = new TokenRuleIterator($input, $pattern);
+
+        $this->assertCount(count($expected), $iterator->getArrayCopy());
+        foreach ($expected as [$offset, $value]) {
+            $this->assertTrue($iterator->valid());
+            $this->assertNotEmpty($iterator->current());
+            $this->assertIsString($iterator->current());
+            $this->assertSame($value, $iterator->current());
+            $this->assertSame($offset, $iterator->key());
+            $iterator->next();
+        }
+    }
+
+    public static function dataProvider(): array
+    {
+        return [
+            [new TokenRulePattern('foo'), 'foobarbaz' ,[[0, 'foo']]],
+            [new TokenRulePattern('foo'), 'barfoobaz', [[3, 'foo']]],
+            [new TokenRulePattern('foo'), 'barbazfoo', [[6, 'foo']]],
+            [new TokenRulePattern('zig'), 'barbazfoo', []],
+            [new TokenRulePattern('zig'), 'zigzaggzigzag', [
+                [0, 'zig'],
+                [7, 'zig'],
+            ]],
+            [new TokenRulePattern('/z.g/'), 'zigzaggzigzag', [
+                [0, 'zig'],
+                [3, 'zag'],               
+                [7, 'zig'],
+                [10, 'zag'],
+            ]],
+            [new TokenRulePattern('/(?<name>(bar)(biz)(boo))/'), 'barbizboooobzibrabbarbizboo', [
+                [0, 'barbizboo'],
+                [18, 'barbizboo'],
+            ]]
+        ];
+    }
+}
