9f046ef2d72dafe3f17262f87a47a09bd98f7943
Joao Barreto <JoaoBarreto255@users.noreply.github.com>
2024-04-01 13:59:22 -0300

    improve: add logic for builder lexer

--- src/Lexer/AbstractLexer.php
+++ src/Lexer/AbstractLexer.php
@@ -9,7 +9,61 @@ use Joaobarreto255\PhpCompBuilder\Lexer\Pattern\TokenRulePattern;
 
 abstract class AbstractLexer
 {
+    private array $patterns;
+    private string $currentInput = '';
+
     public function __construct(
         readonly private \Iterator $streamIterator
-    ) { }
+    ) {
+        $this->patterns = $this->getTokenRuleFromMethods();
+    }
+
+    /** return regex from caracteres to be ignored */
+    abstract public function ignorePattern(): string;
+
+    public function getToken()
+    {
+    }
+
+    private function factoryIteratorsFromInput(): array
+    {
+        $iterators = $this->patterns;
+        foreach ($iterators as $key => $pattern) {
+            $pattern['iterator'] = new TokenRuleIterator($this->currentInput, $pattern['rule']);
+            unset($pattern['rule']);
+            $iterators[$key] = $pattern;
+        }
+
+        return $iterators;
+    }
+
+    /**
+     * @return array - methods with pattern to be processed
+     */
+    private function getTokenRuleFromMethods(): array
+    {
+        $reflection = new \ReflectionClass($this);
+        $methods = $reflection->getMethods(\ReflectionMethod::IS_PROTECTED | \ReflectionMethod::IS_PUBLIC);
+
+        $result = [];
+        foreach ($methods as $reflMethod) {
+            if (empty($tokenRules = $reflMethod->getAttributes(TokenRulePattern::class))) {
+                continue;
+            }
+
+            $methodName = $reflMethod->getName();
+            foreach ($tokenRules as $rule) {
+                $result[] = [
+                    'method' => $methodName,
+                    'tokenRule' => $rule,
+                ];
+            }
+        }
+
+        if ($result) {
+            return $result;
+        }
+
+        throw new \LogicException('Error no methods to fetch pattern', 1);
+    }
 }
