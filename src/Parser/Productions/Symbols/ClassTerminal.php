a42496eb5b0e602c3738c68f67599effc00cf795
Joao Barreto <JoaoBarreto255@users.noreply.github.com>
2024-04-16 23:26:32 -0300

    fix: bugs founded in unittest

--- src/Parser/Productions/Symbols/ClassTerminal.php
+++ src/Parser/Productions/Symbols/ClassTerminal.php
@@ -6,11 +6,12 @@ namespace JB255\PHPCompBuilder\Parser\Productions\Symbols;
 
 class ClassTerminal extends Terminal implements SymbolInterface
 {
-    public const SYMBOL_PATTERN = '/^\'[:alpha:](\w|\_)*(\\\[:alpha:](\w|\_)*)*\'$/i';
+    public const SYMBOL_PATTERN = '/^\'[[:alpha:]](\w|\_)*(\\\[[:alpha:]](\w|\_)*)*\'$/i';
 
     protected function extraValueValidation(string $value): void
     {
-        if (!class_exists($value)) {
+        $classname = trim($value, "'");
+        if (!class_exists($classname)) {
             throw new \InvalidArgumentException("Value isn't a class");
         }
     }
