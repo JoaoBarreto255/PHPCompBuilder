f2cdbeffba4735d8c7c7c0a32eaea795fef4a090
Joao Barreto <JoaoBarreto255@users.noreply.github.com>
2024-05-06 23:09:34 -0300

    test: case for peekRigthToken

--- tests/Lexer/Traits/BuildAndProcessTokenIteratorsTraitTest.php
+++ tests/Lexer/Traits/BuildAndProcessTokenIteratorsTraitTest.php
@@ -6,6 +6,7 @@ namespace JB255\PHPCompBuilder\Tests\Lexer\Traits;
 
 use JB255\PHPCompBuilder\Lexer\Pattern\TokenRuleIterator;
 use JB255\PHPCompBuilder\Lexer\Pattern\TokenRulePattern;
+use JB255\PHPCompBuilder\Lexer\Pattern\TokenState;
 use JB255\PHPCompBuilder\Lexer\Traits\BuildAndProcessTokenIteratorsTrait;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
@@ -27,6 +28,8 @@ class BuildAndProcessTokenIteratorsTraitTest extends TestCase
             new TokenRulePattern('num', '\d+'),
             new TokenRulePattern('__ignoreToken', '/\s+/'),
         ];
+
+        $this->resetColumn()->resetPosition()->setLineno(0)->setLine('');
     }
 
     public function testFactoryIteratorsFromLine()
@@ -40,5 +43,62 @@ class BuildAndProcessTokenIteratorsTraitTest extends TestCase
             $msg = "fail at iterator row ({$key})";
             $this->assertInstanceOf(TokenRuleIterator::class, $iterator, $msg);
         }
+    }
+
+    public function testPeekRightToken(): void
+    {
+        $this->setLine('for forager in Foragers a1 12');
+        $this->factoryIteratorsFromLine();
+
+        $this->assertInstanceOf(TokenState::class, $state = $this->peekRightToken());
+        $this->assertInstanceOf(TokenRulePattern::class, $state->tokenRule);
+        $this->assertSame('for', $state->tokenRule->tokenName);
+        $this->assertSame('for', $state->value);
+        $this->assertSame(3, $state->len);
+        $this->increaseColumn(3)->increasePosition(3);
+
+        $this->assertInstanceOf(TokenState::class, $state = $this->peekRightToken());
+        $this->assertInstanceOf(TokenRulePattern::class, $state->tokenRule);
+        $this->assertSame('__ignoreToken', $state->tokenRule->tokenName);
+        $this->assertSame(' ', $state->value);
+        $this->assertSame(1, $state->len);
+        $this->increaseColumn(1)->increasePosition(1);
+
+        $this->assertInstanceOf(TokenState::class, $state = $this->peekRightToken());
+        $this->assertInstanceOf(TokenRulePattern::class, $state->tokenRule);
+        $this->assertSame('varName', $state->tokenRule->tokenName);
+        $this->assertSame('forager', $state->value);
+        $this->assertSame(7, $state->len);
+        $this->increaseColumn(8)->increasePosition(8);
+
+        $this->assertInstanceOf(TokenState::class, $state = $this->peekRightToken());
+        $this->assertInstanceOf(TokenRulePattern::class, $state->tokenRule);
+        $this->assertSame('in', $state->tokenRule->tokenName);
+        $this->assertSame('in', $state->value);
+        $this->assertSame(2, $state->len);
+        $this->increaseColumn(3)->increasePosition(3);
+
+        $this->assertInstanceOf(TokenState::class, $state = $this->peekRightToken());
+        $this->assertInstanceOf(TokenRulePattern::class, $state->tokenRule);
+        $this->assertSame('func', $state->tokenRule->tokenName);
+        $this->assertSame('Foragers', $state->value);
+        $this->assertSame(8, $state->len);
+        $this->increaseColumn(9)->increasePosition(9);
+
+        $this->assertInstanceOf(TokenState::class, $state = $this->peekRightToken());
+        $this->assertInstanceOf(TokenRulePattern::class, $state->tokenRule);
+        $this->assertSame('varName', $state->tokenRule->tokenName);
+        $this->assertSame('a1', $state->value);
+        $this->assertSame(2, $state->len);
+        $this->increaseColumn(3)->increasePosition(3);
+
+        $this->assertInstanceOf(TokenState::class, $state = $this->peekRightToken());
+        $this->assertInstanceOf(TokenRulePattern::class, $state->tokenRule);
+        $this->assertSame('num', $state->tokenRule->tokenName);
+        $this->assertSame('12', $state->value);
+        $this->assertSame(2, $state->len);
+        $this->increaseColumn(2)->increasePosition(2);
+
+        $this->assertNull($this->peekRightToken());
     }
 }
