7feca288b837f9d7024eda7496d8a65eac3e5c62
Joao Barreto <JoaoBarreto255@users.noreply.github.com>
2024-05-08 21:38:57 -0300

    improve: documentation

--- src/Lexer/LexerTrait.php
+++ src/Lexer/LexerTrait.php
@@ -8,22 +8,13 @@ use JB255\PHPCompBuilder\Lexer\Traits\BuildAndProcessTokenIteratorsTrait;
 use JB255\PHPCompBuilder\Lexer\Traits\LoadTokenRulePatternsTrait;
 
 /**
- * Classe abstrata que fornece uma estrutura básica para criar analisadores lexicais (lexers) em PHP.
+ * Trait that defines a Lexer for a lexer engine.
  *
- * ## Como usar:
+ * This trait provides a base implementation for lexers used in the lexer engine.
+ * It defines methods for initializing the lexer, retrieving the current token,
+ * moving to the next token, and checking for the end of the token stream.
  *
- * 1. **Criar uma classe concreta que estenda AbstractLexer:**
- *    - Implemente o método `ignorePattern()` para retornar a expressão regular dos caracteres a serem ignorados.
- *    - Defina métodos adicionais para processar tokens específicos, utilizando a anotação `@TokenRulePattern` para associar padrões a esses métodos.
- *
- * 2. **Instanciar a classe:**
- *    - Forneça um `\Iterator` contendo as linhas do código de origem como argumento para o construtor.
- *    - Opcionalmente, forneça o nome do arquivo de origem como um segundo argumento.
- *
- * 3. **Iterar sobre os tokens:**
- *    - Utilize os métodos `current()`, `next()`, `key()`, `valid()`, `rewind()`, e outros métodos de iteração para processar os tokens gerados pelo lexer.
- *
- * ## Exemplo:
+ * ## Example:
  *
  * ```php
  * #[TokenRulePattern('identifier', '^[a-zA-Z_]+', MyTokenContainer::class)]
@@ -50,27 +41,42 @@ trait LexerTrait
     use BuildAndProcessTokenIteratorsTrait;
 
     /**
-     * Must be added in __constructor.
-     * 
-     * @param $patterns used to inject other patterns.
+     * Initializes the lexer.
+     *
+     * This method must be called in the constructor of the implementing class.
+     * It takes an iterator for the stream to be lexed, the filename of the stream,
+     * and an optional array of additional token patterns.
+     *
+     * @param \Iterator $streamIterator The iterator for the stream to be lexed.
+     * @param string $filename The filename of the stream.
+     * @param array $patterns An optional array of additional token patterns.
      */
     public function initLexer(
-        \Iterator $streamIterator, string $filename, array $patterns = [],
+        \Iterator $streamIterator, string $filename, array $patterns = []
     ) {
         $this->initTokenPatterns($patterns);
         $this->initTokenStream($streamIterator, $filename);
         $this->tokenStream = $this->buildTokenStream();
     }
 
-    /** 
-     * @return string regex from caracteres to be ignored 
+    /**
+     * Defines the pattern for characters to be ignored during lexing.
+     *
+     * This method must be implemented by the descendant class and should return a regular expression
+     * that matches the characters to be ignored.
+     *
+     * @return string The regular expression for characters to be ignored.
+     * @abstract
      */
     abstract public function ignorePattern(): string;
 
     /**
-     * returns current token.
+     * Returns the current token from the token stream.
      *
-     * @return TokenInterface|int
+     * This method returns the current token from the token stream. The returned value can be either
+     * a TokenInterface object or an integer representing the type of the token.
+     *
+     * @return TokenInterface|string|array|\stdClass The current token from the token stream.
      */
     public function current(): mixed
     {
@@ -78,7 +84,9 @@ trait LexerTrait
     }
 
     /**
-     * Move cursor to next token.
+     * Moves the lexer to the next token in the stream.
+     *
+     * This method advances the internal pointer of the token stream to the next token.
      */
     public function next(): void
     {
@@ -86,7 +94,12 @@ trait LexerTrait
     }
 
     /**
-     * Returns "key", number off tokens processed.
+     * Returns the current key of the token stream.
+     *
+     * This method returns the current key of the token stream, which represents the number of tokens
+     * processed so far.
+     *
+     * @return int The current key of the token stream.
      */
     public function key(): mixed
     {
@@ -94,7 +107,9 @@ trait LexerTrait
     }
 
     /**
-     * rewind lexer to first token.
+     * Rewinds the lexer to the first token in the stream.
+     *
+     * This method resets the internal pointer of the token stream to the beginning.
      */
     public function rewind(): void
     {
@@ -102,7 +117,11 @@ trait LexerTrait
     }
 
     /**
-     * verify if stream of tokens finish.
+     * Checks whether the end of the token stream has been reached.
+     *
+     * This method returns true if the end of the token stream has been reached, false otherwise.
+     *
+     * @return bool True if the end of the token stream has been reached, false otherwise.
      */
     public function valid(): bool
     {
